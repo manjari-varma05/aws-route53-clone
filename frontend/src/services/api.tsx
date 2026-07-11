4e1ec8fff927e7a92db08ae929aeceace28ae81d
manjari-varma05 <manjarivarma05@gmail.com>
2026-07-11 19:04:16 +0530

    Update frontend API URL

--- frontend/src/services/api.tsx
+++ frontend/src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://aws-route53-clone-8.onrender.com",
+    baseURL: "https://aws-route53-clone-13.onrender.com",
     headers: {
         "Content-Type": "application/json",
     },
